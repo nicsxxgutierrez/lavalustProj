a9558b79297ce0f25c885d7b568aaec6e765158b
Nicolle <130277462+nicsxxgutierrez@users.noreply.github.com>
2023-12-26 10:43:27 +0800

    main

--- app/views/venue.php
+++ app/views/venue.php
@@ -9,15 +9,15 @@
   <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
   <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
-  <link href="admin/assets/css/jquery.datetimepicker.min.css" rel="stylesheet">
+  <link href="public/admin/assets/css/jquery.datetimepicker.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-  <link href="admin/assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.css" rel="stylesheet" />
+  <link href="public/admin/assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.css" rel="stylesheet" />
   <link href="public/css/styles.css" rel="stylesheet" />
-  <link href="admin/assets/css/select2.min.css" rel="stylesheet">
-  <script src="admin/assets/vendor/jquery/jquery.min.js"></script>
-  <script src="admin/assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-  <script type="text/javascript" src="admin/assets/js/select2.min.js"></script>
-  <script type="text/javascript" src="admin/assets/js/jquery.datetimepicker.full.min.js"></script>
+  <link href="public/admin/assets/css/select2.min.css" rel="stylesheet">
+  <script src="public/admin/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="public/admin/assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+  <script type="text/javascript" src="public/admin/assets/js/select2.min.js"></script>
+  <script type="text/javascript" src="public/admin/assets/js/jquery.datetimepicker.full.min.js"></script>
 
     <style>
         header.masthead {
